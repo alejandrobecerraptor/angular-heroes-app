19e8e100e40d02cd1475185920f342bc69896221
Alejandro Bece <alejandro.becerra.pro@gmail.com>
2024-11-15 11:48:10 -0500

    guards de angular, implementar funcionalidad guard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-
 export class AuthService {
   private baseUrl: string = environments.baseUrl;
   private user?: User;
@@ -21,6 +20,16 @@ export class AuthService {
     return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
       tap((user) => (this.user = user)),
       tap((user) => localStorage.setItem('token', 'ASDdS4dD435dfdfdg4434Ddd$'))
+    );
+  }
+
+  checkAuthentication(): Observable<boolean> {
+    if (!localStorage.getItem('token')) return of(false);
+    const token = localStorage.getItem('token');
+    return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap((user) => (this.user = user)),
+      map((user) => !!user),
+      catchError(() => of(false))
     );
   }
 
