12984f7b2df8f40bbac82bbed25205a6b972c9aa
Alejandro Bece <alejandro.becerra.pro@gmail.com>
2024-11-15 12:56:12 -0500

    updated docs

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -3,7 +3,7 @@ import { FormControl } from '@angular/forms';
 import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
-
+import { environments } from '../../../../environments/environments.prod';
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
@@ -13,6 +13,7 @@ export class SearchPageComponent {
   searchInput = new FormControl('');
   heroes: Hero[] = [];
   selectedHero?: Hero;
+  isProduction = environments.baseUrl;
 
   constructor(private heroService: HeroesService) {}
 
